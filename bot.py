b805c8ced23acb8ca85a3a78c557a0d07a508bb7
UnaPepsi <pitoparado6669@gmail.com>
2023-12-06 18:48:56 -0600

    Fixed some stuff

--- bot.py
+++ bot.py
@@ -140,7 +140,11 @@ def run_discord_bot():
 			await interaction.response.send_message("Yes, finally!!!")
 	@client.tree.command(description="Shows the bot's changelog")
 	async def changelog(interaction: discord.Interaction):
-		await interaction.response.send_message(botchangelog.changelog("2.3.0"))
+		await interaction.response.send_message(botchangelog.changelog("2.4.0"))
+	@client.tree.command(description="Sends Rats Invaders .apk")
+	async def ratsapk(interaction: discord.Interaction):
+		await interaction.response.defer()
+		await interaction.followup.send(file=discord.File("files/ratsinvaders2.0.apk"))
 	
 	keep_alive()
 	client.run(os.environ['TOKEN'])
