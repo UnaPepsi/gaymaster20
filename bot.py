bcbc6f966c111b0978864c52cd0874585c77483b
UnaPepsi <119690535+UnaPepsi@users.noreply.github.com>
2023-12-11 22:27:05 +0000

    fixes for redditapi

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import discord,os,fortnite,typing,oss,time,botchangelog,randompass,reddit
+import discord,os,fortnite,typing,oss,time,botchangelog,randompass,redditapi
 from random import randint,choice
 from requests import get
 from keep_alive import keep_alive
@@ -176,13 +176,13 @@ def run_discord_bot():
 		await interaction.response.send_message(f"```{randompass.pass_gen(lower,upper,numbers,symbols,length)}```",ephemeral=True)
 	@client.tree.command(description="Shows the bot's changelog")
 	async def changelog(interaction: discord.Interaction):
-		await interaction.response.send_message(botchangelog.changelog("2.8.0"))
+		await interaction.response.send_message(botchangelog.changelog("2.9.0"))
 	@client.tree.command(description="Sends a random copypasta from Reddit")
 	async def copypasta(interaction: discord.Interaction):
 		embed = discord.Embed(
 			colour=discord.Colour.dark_orange(),
 			title="Copypasta",
-			description=reddit.rand_copy_pasta()
+			description=redditapi.rand_copy_pasta()
 		)
 		await interaction.response.send_message(embed=embed)
 
--- reddit.py
+++
@@ -1,8 +0,0 @@
-from requests import get
-
-def rand_copy_pasta():
-	try:
-		randpost = get("https://www.reddit.com/r/copypasta/random.json").json()
-	except KeyError:
-		return "Reddit's api is the most dogshit api that has ever existed and has the worst rate limit ever, so you have to wait to use this command again"
-	return randpost[0]['data']['children'][0]['data']['selftext']
